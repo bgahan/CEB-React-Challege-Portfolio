4df34965def19987cbfa8308a8fcca99ab505ccd
Brittany Gahan <brittanygahan89@gmail.com>
2022-07-29 20:31:07 -0400

    add css styling for header and footer components

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import './header.css'
-import { useLocation } from 'react-router-dom'
+import './header.css';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
 
@@ -12,10 +12,9 @@ const Header = () => {
                 <h1 className='header-title'>My Portfolio</h1>
                 <div>
                     <ul className='header-nav'>
-                        <li><a href="/" className={location.pathname === '/' ? 'current-path' : ''}>Home</a></li>
-                        <li><a href="/about-me" className={location.pathname === '/about-me' ? 'current-path' : ''}>About Me</a></li>
-                        <li><a href="/contact-me" className={location.pathname === '/contact-me' ? 'current-path' : ''}>Contact Me</a></li>
+                        <li><a href="/" className={location.pathname === '/' ? 'current-path' : ''}>About Me</a></li>
                         <li><a href="/portfolio" className={location.pathname === '/portfolio' ? 'current-path' : ''}>Portfolio</a></li>
+                        <li><a href="/contact-me" className={location.pathname === '/contact-me' ? 'current-path' : ''}>Contact Me</a></li>
                         <li><a href="/resume" className={location.pathname === '/resume' ? 'current-path' : ''}>Resume</a></li>
                     </ul>
                 </div>
