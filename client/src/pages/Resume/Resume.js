8f6ba9393370ea7b760478c8fefbbb8b552f90cb
Brittany Gahan <brittanygahan89@gmail.com>
2022-07-30 19:54:59 -0400

    set up resume page

--- client/src/pages/Resume/Resume.js
+++ client/src/pages/Resume/Resume.js
@@ -5,7 +5,27 @@ const Resume = () => {
   return (
     <div className='resume'>
       <h1>Resume</h1>
+      <p>Download my <a href="./resume.pdf" download="resume.pdf">resume</a></p>
       <h2>Skills</h2>
+      <div className='skills'>
+        <ul> Front-End
+          <li>HTML/CSS/Git</li>
+          <li>JavaScript</li>
+          <li>Bootstrap</li>
+          <li>APIs</li>
+          <li>JQuery</li>
+          <li>JSON</li>
+          <li>React</li>
+        </ul>
+        <ul>Back-End
+          <li>Node</li>
+          <li>Object-oriented programming</li>
+          <li>Express</li>
+          <li>MySQL</li>
+          <li>Sequelize</li>
+          <li>MongoDB</li>
+        </ul>
+      </div>
     </div>
   )
 }
