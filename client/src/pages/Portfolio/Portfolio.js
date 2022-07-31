47629173b496b18ebfbc777900a0d4208953195b
Brittany Gahan <brittanygahan89@gmail.com>
2022-07-30 20:01:13 -0400

    initial project data creation

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './portfolio.css';
+import './projects.json';
 
 const Portfolio = () => {
   return (
     <div className='portfolio'>
       <h1>Portfolio</h1>
+      <div className='projects'>
+
+      </div>
     </div>
   )
 }
